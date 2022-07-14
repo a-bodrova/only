41bdcb7813bfaeef6c4dcb43d79b75c261875cba
a-bodrova <soulwent94@gmail.com>
2022-07-14 17:47:12 +0400

    feat: add styles and functionality

--- adaptive-spoiler/src/index.js
+++ adaptive-spoiler/src/index.js
@@ -1,1 +1,19 @@
 import './assets/sass/index.scss';
+
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => {
+  item.onclick = (e) => {
+    const self = e.currentTarget;
+    const content = self.querySelector('.item__content');
+
+    item.classList.toggle('active');
+
+    if (self.classList.contains('active')) {
+      content.style.maxHeight = `${content.scrollHeight}px`;
+    } else {
+      content.style.maxHeight = '0';
+    }
+
+  }
+})
